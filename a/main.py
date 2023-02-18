eb4bb81af6fff33879ffd2546b8b31945c5d6d2c
zhanghed <747988598@qq.com>
2023-02-18 17:17:32 +0800

    w

--- a/main.py
+++ a/main.py
@@ -2,48 +2,56 @@ from tkinter import *
 import time
 
 
-class Win():
+class Win(object):
     def __init__(self):
         self.win = Tk()
-        self.width = 500
-        self.height = 500
-        self.screen_width = self.win.winfo_screenwidth()
-        self.screen_height = self.win.winfo_screenheight()
-        self.center_x = (self.screen_width-self.width)/2
-        self.center_y = (self.screen_height-self.height)/2
-        self.win.geometry("%dx%d+%d+%d" %
-                          (self.width, self.height, self.center_x, self.center_y))
         self.win.title('测试')
         self.win.iconbitmap('a/favicon.ico')
         self.win.resizable(0, 0)
-        self.frame_top = Frame(width=380, height=270, bg='white')
-        self.frame_top.grid(row=0, column=0, padx=5, pady=5)
-        self.frame_top.grid_propagate(0)
-        self.frame_center = Frame(width=380, height=130, bg='white')
-        self.frame_center.grid(row=1, column=0, padx=5, pady=5)
-        self.frame_center.grid_propagate(0)
-        self.frame_right = Frame(width=170, height=400, bg='white')
-        self.frame_right.grid(row=0, column=1, rowspan=3, padx=4, pady=5)
-        self.frame_right.grid_propagate(0)
-        self.frame_bottom = Frame(width=380, height=20)
-        self.frame_bottom.grid(row=2, column=0)
-        self.frame_bottom.grid_propagate(0)
-        self.button_sendmsg = Button(self.frame_bottom, text=('发送'), command=self.send_message)
-        self.button_sendmsg.grid(sticky=E)
-        self.message = Text(self.frame_top)
-        self.message.grid()
-        self.input = Text(self.frame_center)
-        self.input.grid()
+        # 位置居中
+        self.c_x = (self.win.winfo_screenwidth()-500)/2
+        self.c_y = (self.win.winfo_screenheight()-500)/2
+        self.win.geometry("%dx%d+%d+%d" % (500, 500, self.c_x, self.c_y))
+        # 消息列表
+        self.frame_1 = Frame(width=380, height=270, bg='white')
+        self.frame_1.grid(row=0, column=0, padx=5, pady=5)
+        self.frame_1.grid_propagate(0)
+        # 信息输入框
+        self.frame_2 = Frame(width=380, height=130, bg='white')
+        self.frame_2.grid(row=1, column=0, padx=5, pady=5)
+        self.frame_2.grid_propagate(0)
+        # 好友列表
+        self.frame_3 = Frame(width=100, height=490, bg='white')
+        self.frame_3.grid(row=0, column=1, rowspan=3, padx=5, pady=5)
+        self.frame_3.grid_propagate(0)
+        # 发送按钮
+        self.frame_11 = Frame(width=380, height=70, bg='white')
+        self.frame_11.grid(row=2, column=0, padx=5, pady=5)
+        self.frame_11.grid_propagate(0)
+        # 绑定消息列表
+        self.mes_list = Text(self.frame_1, width=52,
+                             height=20, bd=0, padx=5, pady=5,state=DISABLED)
+        self.mes_list.grid(row=0, column=0)
+        # 绑定输入消息内容
+        self.mes_txt = Text(self.frame_2)
+        self.mes_txt.grid()
+        # 绑定好友列表
+        self.friend_list = Text(self.frame_3)
+        self.friend_list.grid()
+        # 板锭发送按钮
+        self.mes_send = Button(self.frame_11, text=('发送'),
+                               command=self.fun_send)
+        self.mes_send.grid()
 
-    def send_message(self):
-        msgcontent = '我: ' + \
-            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n'
-        self.message.insert(END, msgcontent, 'green')
-        self.message.insert(END, self.input.get('0.0', END))
-        self.input.delete('0.0', END)
+    def fun_send(self):
+        t = '我: ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n'
+        self.mes_list["state"] =NORMAL
+        self.mes_list.insert(END, t)
+        self.mes_list.insert(END, self.mes_txt.get('0.0', END))
+        self.mes_txt.delete('0.0', END)
+        self.mes_list["state"] = DISABLED
 
+if __name__ == "__main__":
 
-
-if __name__=="__main__":
-    win=Win().win
+    win = Win().win
     win.mainloop()
