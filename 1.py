1b3eb1613bdf59b372cf2333245ef91714233189
zhanghed <747988598@qq.com>
2023-02-20 21:14:52 +0800

    h

--- 1.py
+++ 1.py
@@ -4,50 +4,65 @@ import threading
 import time
 
 
-# 存放已连接的对象
-clients = []
-# 存放公共消息的容器
-public_message = dict()
-
-
 class Server_thread(threading.Thread):
+    # 服务线程
     def __init__(self, name):
         threading.Thread.__init__(self, name=name)
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((socket.gethostbyname(socket.gethostname()), 9090))
         self.server.listen(5)
-        print(self.server)
+        print("启动：", self.server)
+        # 连接池
         self.clients = {}
+        # 消息池
+        self.publics = {}
 
     def run(self):
         while True:
+            # 监控连接
             client, addr = self.server.accept()
             if not str(client) in self.clients:
+                # 创建接收消息线程
+                recv = Recv_thread(
+                    name="Recv_thread", client=client,  publics=self.publics)
+                recv.start()
+                # 加入连接池
                 self.clients[client] = {"client": client,
                                         "addr": addr,
-                                        "ip": client.getpeername()[0]}
-            threading.Thread(target=recv_thread, args=(client,)).start()
+                                        "ip": client.getpeername()[0],
+                                        "recv": recv}
+                print("接入：", self.clients[client])
             time.sleep(1)
 
 
 class Recv_thread(threading.Thread):
-    def __init__(self,client,name):
+    # 接收消息线程
+    def __init__(self, name,  client, publics):
         threading.Thread.__init__(self, name=name)
-        
+        self.client = client
+        self.publics = publics
+
     def run(self):
         while True:
-            if client in clients:
-                data = client.recv(1024).decode('utf-8')
-                if data != '':
-                    print(data)
-                    public_message[client] = queue.Queue()
-                    public_message[client].put(data)
-                else:
-                    if client in clients:
-                        print("用户优雅的退出了")
-                        clients.remove(client)
+            # 创建消息队列
+            public=self.publics[self.client] = queue.Queue()
+            data=self.client.recv(1024).decode('utf-8')
+            # 加入消息池
+            print(data)
+            public.put(data)
             time.sleep(1)
 
+
+server = Server_thread(name="Server_thread")
+server.start()
+
+while True:
+    # print("clients", len(server.clients), "publics", len(server.publics))
+    # print(len(threading.enumerate()), threading.enumerate())
+    # for k,v in server.publics.items():
+    #     print(k,v)
+
+    time.sleep(5)
 
 # # 转发消息(非/阻塞)
 # def broadcast():
@@ -62,14 +77,3 @@ class Recv_thread(threading.Thread):
 #                         if data != '':
 #                             client.send(bytes(data.encode('utf-8')))
 #                             print('服务器转发了消息')
-
-
-Server_thread(name="Server_thread").start()
-# t2 = threading.Thread(target=broadcast)
-
-# t2.start()
-
-# 主线程监听在线人数
-while (True):
-    print("当前在线人数为:%d" % (len(clients)))
-    time.sleep(5)
